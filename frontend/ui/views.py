64813d28c65c34e51eabfa23f2200890d74ea605
Ankita Mohapatra <anksstudyuse@gmail.com>
2025-12-20 20:55:45 +0530

    Initial Django frontend with image upload UI

---
+++ frontend/ui/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+import requests
+# Create your views here.
+
+
+def index(request):
+    result = None
+
+    if request.method == "POST" and request.FILES.get("image"):
+        image = request.FILES["image"]
+
+        # sending image to ML FastAPI backend
+        response = requests.post(
+            "http://127.0.0.1:8000/moderate-image",
+            files={"file": image}
+        )
+
+        if response.status_code == 200:
+            result = response.json()
+        else:
+            result = {"error": "ML API failed"}
+
+    return render(request, "ui/index.html", {"result": result})
